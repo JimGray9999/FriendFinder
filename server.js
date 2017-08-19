ec830900163cfb06331c957f32b96d52d3361ee2
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-19 12:42:55 -0400

    updates

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ var port = process.env.PORT || 3000;
 
 var app = express(); // create an app instance of express.js
 
-// direct GET request to the  home page
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+// direct GET request to the home page
 app.get('/', function (req, res){
   res.sendfile(__dirname + '/app/public/home.html');
 });
@@ -16,7 +26,7 @@ app.get('/survey.html', function (req, res){
   res.sendfile(__dirname + '/app/public/survey.html');
 });
 
-// event listener on port 8080
+// event listener on port 3000 (required for Heroku app)
 app.listen(port, function(){
-  console.log("Shhh, were listening on port 8080");
+  console.log("Shhh, were listening on port 3000");
 });
