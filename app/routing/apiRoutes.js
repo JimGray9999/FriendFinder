f9fc3550e404bd66684352eb95bf1f12ab948465
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-24 07:46:05 -0400

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,19 +12,29 @@ module.exports = function(app) {
 
   // use the POST request to add to the friends.js
   app.post("/api/new", function(req, res) {
+    var newFriend = findAfriend(friends, req.body);
     friends.push(req.body);
-    findAfriend(friends);
-    res.json(friends, friends[friends.length-1]);
+    res.json(newFriend);
   })
 
   function findAfriend (friends, you) {
-    var diffScore = [];
-    var diff = 0;
+    var diffScore = 0;
+    var bestDiff = 100;
+    var newBFF = "";
 
     for (i = 0; i < friends.length ; i++) {
       for (j = 0 ; j < 10 ; j++) {
-        diffScore.push(Math.abs(friends[i].scores[j] - you.scores[j]));
+        diffScore += (Math.abs(friends[i].scores[j] - you.scores[j]));
+      }
+      if (diffScore < bestDiff) {
+        bestDiff = diffScore;
+        diffScore = 0;
+        newBFF = friends[i];
       }
     }
+    console.log("diffScore: " + diffScore);
+    console.log("bestDiff: "+ bestDiff);
+    console.log("New BFF: " + newBFF.name);
+    return newBFF;
   }
 };
